5680026047fcba6d1b9db6d6f092f9b20d8b9bfa
can.yuva <can.yuva@obss.com.tr>
2023-09-27 11:20:20 +0300

    PDF confis

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const savePDF = () => {
   var element = document.body;
   var opt = {
-    margin: [0, 0, 30, 0], //top, left, buttom, right,
-    filename: 'my_file.pdf',
+    margin: [0, 0, 30, 30], //top, left, buttom, right,
+    filename: 'Can YUVA - CV.pdf',
     image: { type: 'jpeg', quality: 0.98 },
-    html2canvas: { dpi: 192, scale: 2, letterRendering: true },
-    pagebreak: { mode: 'avoid-all' },
+    html2canvas: { dpi: 192, scale: 1, letterRendering: true },
     jsPDF: { unit: 'pt', format: 'a4', orientation: 'portrait' }
   };
   var worker = html2pdf();
