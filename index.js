f650ce6ce6a20e4876af4d2d30a71ec89aae06d1
can.yuva <can.yuva@obss.com.tr>
2023-09-27 10:27:43 +0300

    PDF export

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-var element = document.getElementById('export-section');
-var opt = {
-  margin:       1,
-  filename:     'Can YUVA - CV.pdf',
-  image:        { type: 'jpeg', quality: 0.98 },
-  html2canvas:  { scale: 2 },
-  jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
-};
-
-function savePDF () {
-    html2pdf().set(opt).from(element).save();
+const savePDF = () => {
+  var element = document.body;
+  var opt = {
+    margin: [0, 0, 30, 0], //top, left, buttom, right,
+    filename: 'my_file.pdf',
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { dpi: 192, scale: 2, letterRendering: true },
+    pagebreak: { mode: 'avoid-all' },
+    jsPDF: { unit: 'pt', format: 'a4', orientation: 'portrait' }
+  };
+  var worker = html2pdf();
+  
+  worker.set(opt)
+    .from(element)
+    .toPdf()
+    .get('pdf')
+    .save();
 }
